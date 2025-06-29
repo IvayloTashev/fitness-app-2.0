55280f8b2af37a326b23be86fda2e41806549224
IvayloTashev <i.tashev@abv.bg>
2025-06-29 13:21:47 +0300

    Initialize the project

---
+++ client/src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+
+  return (
+    <>
+      <div className='bg-gray-100 text-3xl font-dmsans'>Whereas disregard and contempt for human rights have resulted</div>
+    </>
+  );
+}
+
+export default App;
