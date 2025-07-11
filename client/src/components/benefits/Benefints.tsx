d1ef7bf2690cdf8201a7936439ac2afd243605fc
IvayloTashev <i.tashev@abv.bg>
2025-07-11 19:56:44 +0300

    Finalize benefits component

--- client/src/components/benefits/Benefints.tsx
+++ client/src/components/benefits/Benefints.tsx
@@ -8,7 +8,11 @@ import {
 import { motion } from "framer-motion";
 import HText from "../../shared/HText";
 import Benefit from "./Benefit";
+import AbstractWaves from "../../assets/AbstractWaves.png";
+import Sparkles from "../../assets/Sparkles.png";
+import BenefitsPageGraphic from "../../assets/BenefitsPageGraphic.png";
 import { benefitsContent } from "../../shared/utils";
+import ActionButton from "../../shared/ActionButton";
 
 const container = {
   hidden: {},
@@ -62,6 +66,77 @@ const Benefints = ({ setSelectedPage }: Props) => {
             />
           ))}
         </motion.div>
+
+        <div className="mt- items-center justify-between gap-20 md:mt-28 flex-md">
+          <img
+            className="mx-auto"
+            src={BenefitsPageGraphic}
+            alt="benefitsPageGraphic"
+          />
+
+          <div>
+            <div>
+              <div className="relative">
+                <motion.div
+                  initial="hidden"
+                  whileInView="visible"
+                  viewport={{ once: true, amount: 0.5 }}
+                  transition={{ duration: 0.5 }}
+                  variants={{
+                    hidden: { opacity: 0, x: 50 },
+                    visible: { opacity: 1, x: 0 },
+                  }}
+                >
+                  <img
+                    src={AbstractWaves}
+                    alt="abstractWaves"
+                    className="absolute -top-20 -left-20 z-1"
+                  />
+
+                  <HText>
+                    MILLIONS OF HAPPY MEMBERS GETTING{" "}
+                    <span className="text-primary-500">FIT</span>
+                  </HText>
+                </motion.div>
+              </div>
+            </div>
+
+            <motion.div
+              initial="hidden"
+              whileInView="visible"
+              viewport={{ once: true, amount: 0.5 }}
+              transition={{ delay: 0.2, duration: 0.5 }}
+              variants={{
+                hidden: { opacity: 0, x: 50 },
+                visible: { opacity: 1, x: 0 },
+              }}
+            >
+              <p className="my-5">
+                Nascetur aenean massa auctor tincidunt. Iaculis potenti amet
+                egestas ultrices consectetur adipiscing ultricies enim. Pulvinar
+                fames vitae vitae quis. Quis amet vulputate tincidunt at in
+                nulla nec. Consequat sed facilisis dui sit egestas ultrices
+                tellus. Ullamcorper arcu id pretium sapien proin integer nisl.
+                Felis orci diam odio.
+              </p>
+              <p className="mb-5">
+                Fringilla a sed at suspendisse ut enim volutpat. Rhoncus vel est
+                tellus quam porttitor. Mauris velit euismod elementum arcu neque
+                facilisi. Amet semper tortor facilisis metus nibh. Rhoncus sit
+                enim mattis odio in risus nunc.
+              </p>
+            </motion.div>
+
+            <div className="relative">
+              <div className="absolute -bottom-20 right-100 z-[-1]">
+                <img src={Sparkles} alt="sparkles" />
+              </div>
+              <ActionButton setSelectedPage={setSelectedPage}>
+                Join Now
+              </ActionButton>
+            </div>
+          </div>
+        </div>
       </motion.div>
     </section>
   );
